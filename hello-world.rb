6f66aaf739256f987378194a54056e35d55befb1
Will Lewis <will@wflewis.com>
2019-01-15 17:55:26 -0500

    Make a change

--- hello-world.rb
+++ hello-world.rb
@@ -2,6 +2,6 @@ require 'json'
 
 def lambda_handler(event:, context:)
     # TODO implement
-    { statusCode: 200, body: JSON.generate('Hello from Lambda, again!') }
+    { statusCode: 200, body: JSON.generate('Hello! Third time\'s the charm!') }
 end
 
